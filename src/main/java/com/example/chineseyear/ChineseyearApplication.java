f78451e88a3b29e438f637e5065deebcec44d03b
Mathilde <mathilde.nadeau@hotmail.fr>
2023-03-13 10:31:24 +0100

    init project

---
+++ src/main/java/com/example/chineseyear/ChineseyearApplication.java
@@ -0,0 +1,13 @@
+package com.example.chineseyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChineseyearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChineseyearApplication.class, args);
+	}
+
+}
